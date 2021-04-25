cb9456b620a7283bac30020bf574a16d35a487c3
V1leDev <alexander02plaikner.ap@gmail.com>
2021-04-25 15:58:19 +0200

    Connected View with Controller

--- app/src/main/java/com/example/tictactoemvc/controller/MainActivity.kt
+++ app/src/main/java/com/example/tictactoemvc/controller/MainActivity.kt
@@ -2,22 +2,33 @@ package com.example.tictactoemvc.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.room.Room
 import com.example.tictactoemvc.R
 import com.example.tictactoemvc.model.AppDatabase
 import com.example.tictactoemvc.view.GameView
 import com.example.tictactoemvc.view.GameViewImp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GameView.FieldSelectedListener, GameView.ReadyClickedListener {
 
     private lateinit var usedGameView: GameView
 
     private lateinit var db: AppDatabase
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        usedGameView = GameViewImp(layoutInflater)
+        setContentView(usedGameView.getRootView())
+        usedGameView.setFieldListener(this)
+        usedGameView.setReadyListener(this)
+    }
 
+    override fun onFieldSelected() {
+        Log.d("debug", "field selected")
+    }
 
-        setContentView(R.layout.activity_main)
-
+    override fun onReadyClicked() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/tictactoemvc/view/GameViewImp.kt
+++ app/src/main/java/com/example/tictactoemvc/view/GameViewImp.kt
@@ -1,4 +1,80 @@
 package com.example.tictactoemvc.view
 
-class GameViewImp {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import com.example.tictactoemvc.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class GameViewImp(layoutInflater: LayoutInflater) : GameView, View.OnClickListener {
+    private var rootView = layoutInflater.inflate(R.layout.activity_main, null)
+    var fieldSelectedListener: GameView.FieldSelectedListener? = null
+    var readyClickedListener: GameView.ReadyClickedListener? = null
+
+    init {
+        rootView.findViewById<Button>(R.id.ready1).setOnClickListener {
+            Log.d("debug", "ready1")
+            readyClickedListener?.onReadyClicked()
+        }
+
+        rootView.findViewById<Button>(R.id.ready2).setOnClickListener {
+            Log.d("debug", "ready2")
+            readyClickedListener?.onReadyClicked()
+        }
+
+        rootView.findViewById<Button>(R.id.buttonField1).setOnClickListener {
+            Log.d("debug", "field1 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+        rootView.findViewById<Button>(R.id.buttonField2).setOnClickListener {
+            Log.d("debug", "field2 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+        rootView.findViewById<Button>(R.id.buttonField3).setOnClickListener {
+            Log.d("debug", "field3 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+        rootView.findViewById<Button>(R.id.buttonField4).setOnClickListener {
+            Log.d("debug", "field4 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+        rootView.findViewById<Button>(R.id.buttonField5).setOnClickListener {
+            Log.d("debug", "field5 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+        rootView.findViewById<Button>(R.id.buttonField6).setOnClickListener {
+            Log.d("debug", "field6 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+        rootView.findViewById<Button>(R.id.buttonField7).setOnClickListener {
+            Log.d("debug", "field7 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+        rootView.findViewById<Button>(R.id.buttonField8).setOnClickListener {
+            Log.d("debug", "field8 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+        rootView.findViewById<Button>(R.id.buttonField9).setOnClickListener {
+            Log.d("debug", "field9 selected")
+            fieldSelectedListener?.onFieldSelected()
+        }
+    }
+
+
+
+    override fun getRootView(): View = rootView
+
+    override fun setFieldListener(listener: GameView.FieldSelectedListener) {
+        fieldSelectedListener = listener
+    }
+
+    override fun setReadyListener(listener: GameView.ReadyClickedListener) {
+        readyClickedListener = listener
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
 }
