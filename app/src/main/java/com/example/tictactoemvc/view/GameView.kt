392a596be3721d648dd9bf4e914ca0589ffecd2a
V1leDev <alexander02plaikner.ap@gmail.com>
2021-04-25 15:21:14 +0200

    Update GUI and add some GameView interfaces

--- app/src/main/java/com/example/tictactoemvc/view/GameView.kt
+++ app/src/main/java/com/example/tictactoemvc/view/GameView.kt
@@ -1,4 +1,18 @@
 package com.example.tictactoemvc.view
 
+import android.view.View
+
 interface GameView {
+    fun getRootView(): View
+
+    interface FieldSelectedListener {
+        fun onFieldSelected()
+    }
+
+    interface ReadyClickedListener {
+        fun onReadyClicked()
+    }
+
+    fun setFieldListener(listener: FieldSelectedListener)
+    fun setReadyListener(listener: ReadyClickedListener)
 }
